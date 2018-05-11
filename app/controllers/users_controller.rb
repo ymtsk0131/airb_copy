8bc6fdc0b410805f22a52ee9d6197a7d42b8fbbf
motosaka <ymtsk0131@live.jp>
2018-05-11 22:29:57 +0900

    set controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user.update(user_prams)
   end
 
   def destroy
+    @user.destroy
+    redirect_to root
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :birthday, :gender, :phone, :language, :currency, :address, :introduction)
   end
 
 end
