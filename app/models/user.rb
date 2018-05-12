c20cd824998a3d177fa2ad53a7027648860e5abc
motosaka <ymtsk0131@live.jp>
2018-05-13 01:00:28 +0900

    create user_images model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :user_images, dependent: :destroy
 
   enum gender:{男性: 0, 女性: 1, その他: 2}
   enum language:{日本語: 0, English: 1, 繁体字: 2, 簡体字: 3}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508133002) do
+ActiveRecord::Schema.define(version: 20180512154625) do
+
+  create_table "user_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content",    null: false
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_images_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "firstname",                                         null: false
@@ -38,4 +46,5 @@ ActiveRecord::Schema.define(version: 20180508133002) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "user_images", "users"
 end
