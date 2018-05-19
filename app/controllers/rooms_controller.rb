6e51c71e6fc35924ee1894df08004c0c48a08a8f
motosaka <ymtsk0131@live.jp>
2018-05-19 17:12:54 +0900

    Room information registration function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,75 @@
 class RoomsController < ApplicationController
 
-def index
-end
+  def index
+  end
 
+  def new
+    @room = Room.new
+    @room.build_amenity
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:category,
+                                 :property_type,
+                                 :room_type,
+                                 :occupied,
+                                 :capacity,
+                                 :bedrooms,
+                                 :beds,
+                                 :bathrooms,
+                                 :bathroom_private,
+                                 :location,
+                                 :summary,
+                                 :family,
+                                 :group,
+                                 :pets,
+                                 :other,
+                                 :reservation_deadline,
+                                 :check_in_from,
+                                 :check_in_to,
+                                 :min_stay,
+                                 :max_stay,
+                                 :base_price,
+                                 amenity_attributes:[:essentials,
+                                                     :wifi,
+                                                     :shampoo,
+                                                     :closet,
+                                                     :tv,
+                                                     :heat,
+                                                     :air_condditioner,
+                                                     :breakfast,
+                                                     :workspace,
+                                                     :fireplace,
+                                                     :iron,
+                                                     :hair_dryer,
+                                                     :animals,
+                                                     :private_entrance,
+                                                     :smoke_detector,
+                                                     :carbon_monoxide_detector,
+                                                     :first_aid_kit,
+                                                     :safety_card,
+                                                     :fire_extinguisher,
+                                                     :bedroom_lock,
+                                                     :private_livingroom,
+                                                     :pool,
+                                                     :kitchen,
+                                                     :laundry_washer,
+                                                     :laundry_dryer,
+                                                     :parking,
+                                                     :elevator,
+                                                     :hot_tub,
+                                                     :gym,
+                                                     :room_id]
+                                ).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'rooms#index'
   resources :users, only: %i(show edit update destroy)
+  resources :rooms, only: %i(new create)
   resources :user_images, only: %i(new create destroy)
 end
