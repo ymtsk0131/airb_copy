8b99d5bd482c06a4e9f7ae952aa11d2d9ac6e9a2
motosaka <ymtsk0131@live.jp>
2018-05-21 10:07:52 +0900

    set mvc

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'rooms#index'
   resources :users, only: %i(show edit update destroy)
   resources :rooms, only: %i(new create)
+  resources :rooms do
+    collection do
+      post 'confirm'
+    end
+  end
   resources :user_images, only: %i(new create destroy)
 end
