606b344e28e09bf82c10ad6563dfcc282c010ac2
motosaka <ymtsk0131@live.jp>
2018-05-24 20:20:31 +0900

    set mvc

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!, except: %i(index)
-  before_action :set_new_room, only: %i(confirm create)
+  before_action :set_new_room, only: %i(create)
+  before_action :set_room, only: %i(show edit update)
 
   def index
   end
@@ -10,17 +11,30 @@ class RoomsController < ApplicationController
     @room.build_amenity
   end
 
-  def confirm
-    render :new if @room.invalid?
+  def show
   end
 
   def create
-    if params[:back]
-      render :new
-    elsif @room.save
-      redirect_to root_path
+    if @room.save
+      redirect_to @room
     else
       render :new
+    end
+  end
+
+  def edit
+    @images = @room.room_images
+    @images.build
+    @status = params[:status]
+  end
+
+  def update
+    if params[:flag2]
+      session[:flag2] = "on"
+    end
+    if @room.user_id == current_user.id
+      @room.update(room_params)
+      redirect_to @room
     end
   end
 
@@ -28,6 +42,10 @@ class RoomsController < ApplicationController
 
   def set_new_room
     @room = Room.new(room_params)
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 
   def room_params
@@ -52,6 +70,7 @@ class RoomsController < ApplicationController
                                  :min_stay,
                                  :max_stay,
                                  :base_price,
+                                 :status,
                                  amenity_attributes:[:essentials,
                                                      :wifi,
                                                      :shampoo,
@@ -81,7 +100,12 @@ class RoomsController < ApplicationController
                                                      :elevator,
                                                      :hot_tub,
                                                      :gym,
-                                                     :room_id]
+                                                     :room_id],
+                                 room_images_attributes:[:id,
+                                                         :content,
+                                                         :status,
+                                                         :room_id,
+                                                         :content_cache]
                                 ).merge(user_id: current_user.id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -7,8 +7,14 @@ class Room < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :room_images, dependent: :destroy
   accepts_nested_attributes_for :amenity
+  accepts_nested_attributes_for :room_images, reject_if: :reject_room_images
 
   enum category:{マンション・アパート:0, 住宅:1, サブユニット:2, ユニークな施設:3,　B＆B:4, デザイナーズホテル他:5}
   enum propeety_type:{建物タイプ１:0, 建物タイプ２:1, 建物タイプ３:2, 建物タイプ４:3, 建物タイプ５:4}
   enum room_type:{住宅全体:0,　個室:1,　シェアルーム:2}
+  enum status:{公開:0, 非公開:1}
+
+  def reject_room_images(attributed)
+    attributed['content'].blank?
+  end
 end
