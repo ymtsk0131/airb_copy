a1da7ab8af17f4cb577fbc3977830aae544b38c2
motosaka <ymtsk0131@live.jp>
2018-05-31 23:47:06 +0900

    create pagination

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,7 @@ class RoomsController < ApplicationController
   before_action :set_room, only: %i(show destroy)
 
   def index
-    @rooms = Room.order('created_at DESC').limit(8)
+    @rooms = Room.order('created_at DESC').page(params[:page]).per(8)
   end
 
   def new
